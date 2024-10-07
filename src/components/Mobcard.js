0b2cd8425457da8f9b88ceb593d835cb0d8e99e1
akansh agarwal <akanshagarwal.alwar@gmail.com>
2024-10-07 19:23:14 +0530

    slider

--- src/components/Mobcard.js
+++ src/components/Mobcard.js
@@ -5,38 +5,45 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards } from "swiper/modules";
-
+import nationalSlider from "../img/nationalSlider.jpg";
 export default function Card() {
-    const cardData = product_card_1.map((item) => (
-        <SwiperSlide key={item.id}>
-            <div className="product-card">
-                {item.label ? <div className="dealTag">{item.label}</div> : ""}
-                <div className="product_card_info">
-                    <div
-                        className="product_image"
-                        style={{ backgroundImage: `url(${item.image})` }}
-                    >
-                        <a href={item.url} title={item.title}></a>
-                    </div>
-                    <div className="product_wrapper">
-                        <a href={item.url}>
-                            <div className="product_name">{item.title}</div>
-                        </a>
-                        <div className="new-price">
-                            {item.currency} {item.offerPrice}
-                        </div>
-                    </div>
-                </div>
+  const cardData = product_card_1.map((item) => (
+    <SwiperSlide key={item.id}>
+      <div className="product-card">
+        {item.label ? <div className="dealTag">{item.label}</div> : ""}
+        <div className="product_card_info">
+          <div
+            className="product_image"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <a href={item.url} title={item.title}></a>
+          </div>
+          <div className="product_wrapper">
+            <a href={item.url}>
+              <div className="product_name">{item.title}</div>
+            </a>
+            <div className="new-price">
+              {item.currency} {item.offerPrice}
             </div>
-        </SwiperSlide>
-    ));
-
-    return (
-        <div className="card_slider_container">
-            <p className="list_title">National Packages</p>
-            <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-                {cardData}
-            </Swiper>
+          </div>
         </div>
-    );
+      </div>
+    </SwiperSlide>
+  ));
+
+  return (
+    <div className="card_slider_container">
+      <p className="w-full text-center flex font-bold text-xl mb-4 items-center justify-center">
+        National Packages
+      </p>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        {cardData}
+      </Swiper>
+    </div>
+  );
 }
--- src/components/Mobcardinter.js
+++ src/components/Mobcardinter.js
@@ -7,36 +7,43 @@ import "swiper/css/effect-cards";
 import { EffectCards } from "swiper/modules";
 
 export default function Card() {
-    const cardData = product_card_2.map((item) => (
-        <SwiperSlide key={item.id}>
-            <div className="product-card">
-                {item.label ? <div className="dealTag">{item.label}</div> : ""}
-                <div className="product_card_info">
-                    <div
-                        className="product_image"
-                        style={{ backgroundImage: `url(${item.image})` }}
-                    >
-                        <a href={item.url} title={item.title}></a>
-                    </div>
-                    <div className="product_wrapper">
-                        <a href={item.url}>
-                            <div className="product_name">{item.title}</div>
-                        </a>
-                        <div className="new-price">
-                            {item.currency} {item.offerPrice}
-                        </div>
-                    </div>
-                </div>
+  const cardData = product_card_2.map((item) => (
+    <SwiperSlide key={item.id}>
+      <div className="product-card">
+        {item.label ? <div className="dealTag">{item.label}</div> : ""}
+        <div className="product_card_info">
+          <div
+            className="product_image"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <a href={item.url} title={item.title}></a>
+          </div>
+          <div className="product_wrapper">
+            <a href={item.url}>
+              <div className="product_name">{item.title}</div>
+            </a>
+            <div className="new-price">
+              {item.currency} {item.offerPrice}
             </div>
-        </SwiperSlide>
-    ));
-
-    return (
-        <div className="card_slider_container">
-            <p className="list_title">International Packages</p>
-            <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-                {cardData}
-            </Swiper>
+          </div>
         </div>
-    );
+      </div>
+    </SwiperSlide>
+  ));
+
+  return (
+    <div className="card_slider_container">
+      <p className="w-full text-center flex font-bold text-xl mb-4 items-center justify-center">
+        International Packages
+      </p>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        {cardData}
+      </Swiper>
+    </div>
+  );
 }
