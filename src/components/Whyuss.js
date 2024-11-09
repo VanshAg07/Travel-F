b9ec9fa8b3c719bc6112e20e2139a670ceaf3b41
VanshAg07 <vanshagarwal.alwar007@gmail.com>
2024-11-10 00:44:04 +0530

    whyuss

--- src/components/Whyuss.js
+++ src/components/Whyuss.js
@@ -1,43 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import icon1 from "../img/1why.png";
 import icon2 from "../img/2why.png";
 import icon3 from "../img/3why.png";
 import icon4 from "../img/4why.png";
 
 const WhyTravello = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 425);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 425);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const reasons = [
     {
       title: "No Unnecessary Add-ons",
       description:
         "100% In-House Expertise on Every Adventure! No Outsiders, Just Quality!",
       image: icon1,
+      marginTop: isMobile ? "-20px" : "0px", // Apply custom margin only on small screens
     },
     {
       title: "Clarity & Trust",
       description:
         "Live Monitoring of All Trips—Stay Informed with Up-to-Date Routes and Weather Conditions!",
       image: icon2,
+      marginTop: isMobile ? "-20px" : "0px", // Apply custom margin only on small screens
     },
     {
       title: "Custom Co-Travelers Selection",
       description:
         "Connecting You with the Perfect Travel Companions—Handpicked for Compatibility, Shared Interest.",
       image: icon3,
-      marginTop: "-25px",
+      marginTop: "-50px",
     },
     {
       title: "Your Stress-Free Travel Solution",
       description:
         "Luxury Stays, Trained Drivers, Friendly Crew, and Experienced Trip Leaders—Crafting Unforgettable Memories!",
       image: icon4,
-      marginTop: "-30px",
+      marginTop: "-40px",
     },
   ];
 
   return (
     <div className="why-travello-container mt-80 px-4 w-[90vw] mx-auto relative">
-      {" "}
-      {/* Set width to 90vw and center */}
       <h2 className="text-center md:text-2xl text-xl lg:text-4xl font-bold md:mb-8 mb-4">
         Why Travello10?
       </h2>
@@ -45,7 +55,7 @@ const WhyTravello = () => {
         {reasons.map((reason, index) => (
           <div
             key={index}
-            className="w-full lg:w-1/4 h-[52vh] md:h-[55vh] bg-white p-6 rounded-lg shadow-lg shadow-black flex flex-col justify-between"
+            className="w-full lg:w-1/4 h-[48vh] md:h-[55vh] bg-white p-6 rounded-lg shadow-lg shadow-black flex flex-col justify-between"
           >
             <div>
               <h3 className="md:text-2xl text-lg text-[#03346e] font-semibold mb-2">
