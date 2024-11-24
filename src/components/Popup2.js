9b142431774ef394e0e1f60f14dbd2e8149c67ec
VanshAg07 <vanshagarwal.alwar007@gmail.com>
2024-11-24 21:28:08 +0530

    popups

--- src/components/Popup2.js
+++ src/components/Popup2.js
@@ -8,7 +8,7 @@ const Popup2 = ({ onClose }) => {
 
   const fetchPopup = async () => {
     try {
-      const res = await fetch("https://api.travello10.com/api/popup/assist-user");
+      const res = await fetch("http://localhost:5000/api/popup/assist-user");
       const data = await res.json();
       setPopupData(data[0]);
     } catch (error) {
@@ -31,7 +31,7 @@ const Popup2 = ({ onClose }) => {
 
     try {
       const res = await axios.post(
-        "https://api.travello10.com/api/popup/assist-form",
+        "http://api.travello10.com/api/popup/assist-form",
         formData
       );
       console.log("Form submitted successfully:", res);
@@ -46,13 +46,8 @@ const Popup2 = ({ onClose }) => {
   }
 
   return (
-<<<<<<< HEAD
-    <div className="fixed inset-0 flex items-center justify-center bg-[#ffffff7f] bg-opacity-70 z-50 p-2">
-      <div className="bg-white rounded-lg shadow-lg relative flex flex-col md:flex-row w-full max-w-4xl overflow-hidden">
-=======
     <div className="fixed popup-wr inset-0 flex md:mt-0 -mt-11 items-center justify-center bg-[#ffffff7f] bg-opacity-70 z-50 p-4">
       <div className="bg-white mt-10 rounded-lg md:h-[420px] h-[590px] shadow-lg relative flex flex-col md:flex-row w-full max-w-4xl overflow-hidden">
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
         {/* Close button */}
         <button
           onClick={onClose}
@@ -62,11 +57,7 @@ const Popup2 = ({ onClose }) => {
         </button>
 
         {/* Left side - Image */}
-<<<<<<< HEAD
-        <div className="md:w-1/2 w-full h-48 md:h-auto">
-=======
         <div className="md:w-1/2 md:h-[420px] h-[220px] ">
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
           <div
             className="w-full h-full"
             style={{
@@ -78,73 +69,44 @@ const Popup2 = ({ onClose }) => {
         </div>
 
         {/* Right side - Content */}
-<<<<<<< HEAD
-        <div className="md:w-1/2 w-full p-4 md:p-6 flex flex-col">
-          <h2 className="text-xl md:text-2xl font-bold mb-4 md:mb-6 text-cyan-600">
-            {popupData.title}
-          </h2>
-
-          <form onSubmit={submitForm} className="flex flex-col space-y-4">
-=======
         <div className="md:w-1/2 md:h-[420px] h-[320px] p-6 flex flex-col">
           <h2 className="text-2xl font-bold mb-6 text-cyan-600">
             {popupData.title}
           </h2>
 
           <form onSubmit={submitForm} className="flex flex-col space-y-4 md:pt-10 pt-2">
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
             <input
               type="text"
               name="name"
               placeholder="Name"
-<<<<<<< HEAD
-              className="px-4 py-2 text-sm md:text-base border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-=======
               className="px-4 py-2 text-sm border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
               required
             />
             <input
               type="email"
               name="email"
               placeholder="Email Address"
-<<<<<<< HEAD
-              className="px-4 py-2 text-sm md:text-base border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-=======
               className="px-4 py-2 text-sm border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
               required
             />
             <input
               type="number"
               name="number"
               placeholder="Phone Number"
-<<<<<<< HEAD
-              className="px-4 py-2 text-sm md:text-base border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-=======
               className="px-4 py-2 text-sm border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
               required
             />
             <input
               type="text"
               name="interestedPlaces"
               placeholder="Interested Places"
-<<<<<<< HEAD
-              className="px-4 py-2 text-sm md:text-base border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-=======
               className="px-4 py-2 text-sm border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
               required
             />
 
             <button
               type="submit"
-<<<<<<< HEAD
-              className="mt-2 md:mt-4 px-4 md:px-6 py-2 md:py-3 bg-cyan-500 text-white rounded-lg hover:bg-cyan-600 transition duration-300 shadow-md"
-=======
               className="mt-4 px-6 py-3 bg-cyan-500 text-white rounded-lg hover:bg-cyan-600 transition duration-300 shadow-md"
->>>>>>> ece9787857989f025b443698a72eb96319e41d40
             >
               Submit
             </button>
