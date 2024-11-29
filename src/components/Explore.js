7d0365a667a40e377fb6a4918945996848721444
akansh agarwal <akanshagarwal.alwar@gmail.com>
2024-11-30 02:40:39 +0530

    upcomg

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 import axios from "axios";
-import "./Explore.css"
+import "./Explore.css";
 
 const ImageSlider = () => {
   const [index, setIndex] = useState(0);
@@ -70,9 +70,9 @@ const ImageSlider = () => {
                   key={adventure._id}
                   className="w-1/4 box-border p-2 relative"
                 >
-                  <a
+                  {/* <a
                     href={`/${adventure.title.toLowerCase().replace(" ", "")}`}
-                  >
+                  > */}
                     <video
                       src={adventure.video[0]}
                       alt={`Slide ${i}`}
@@ -89,13 +89,10 @@ const ImageSlider = () => {
                         e.target.style.opacity = 0.8;
                       }}
                     />
-                    <h1
-  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl shadow-lg text-center p-4 w-32 h-32 rounded-full bg-[#00000082] flex items-center justify-center custom-dashed-border"
->
-  {adventure.title}
-</h1>
-
-                  </a>
+                    <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl shadow-lg text-center p-4 w-32 h-32 rounded-full bg-[#00000082] flex items-center justify-center custom-dashed-border">
+                      {adventure.title}
+                    </h1>
+                  {/* </a> */}
                 </div>
               ))}
           </div>
