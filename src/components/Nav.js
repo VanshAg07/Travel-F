1f1b5d8fb9a111ab37bb00eee98fa2c80a86be7f
akansh agarwal <akanshagarwal.alwar@gmail.com>
2024-12-18 12:48:06 +0530

    nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -164,7 +164,7 @@ const Nav = () => {
             </a>
 
             {/* Signin logout */}
-            {/* <div className="desktop-only">
+            <div className="desktop-only">
               {username ? (
                 <div className="flex flex-row items-center">
                   <div className="username-display">
@@ -192,7 +192,7 @@ const Nav = () => {
                   </Link>
                 </div>
               )}
-            </div> */}
+            </div>
           </>
         )}
         {isMobile && (
@@ -540,21 +540,24 @@ const Nav = () => {
             ""
           )} 
         </div> */}
+
         <div className="desktop-only">
           {username ? (
-            <div className="flex flex-row items-center">
-              <div className="username-display">
-                <strong className="mr-2">{username}!</strong>
-              </div>
-              <button className="Btn-n" onClick={handleLogout}>
-                <div className="sign-n">
-                  <svg viewBox="0 0 512 512">
-                    <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
-                  </svg>
+            <>
+              {/* <div className="flex flex-row items-center">
+                <div className="username-display">
+                  <strong className="mr-2">{username}!</strong>
                 </div>
-                <div className="text-n">Logout</div>
-              </button>
-            </div>
+                <button className="Btn-n" onClick={handleLogout}>
+                  <div className="sign-n">
+                    <svg viewBox="0 0 512 512">
+                      <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                    </svg>
+                  </div>
+                  <div className="text-n">Logout</div>
+                </button>
+              </div> */}
+            </>
           ) : (
             <div className={`flex lappi-nav justify-center flex-row `}>
               {/* <a href="tel:+918287804197" onClick={() => setIsMenuOpen(false)}>
